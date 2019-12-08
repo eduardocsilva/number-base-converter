10c190f038a8a6560ab042756195017cba1c6186
Eduardo Silva <1180881@ISEP.IPP.PT>
2019-12-08 23:36:13 +0000

    Update - The number is now converted as the user is typing it

--- scripts/baseConverter.js
+++ scripts/baseConverter.js
@@ -3,7 +3,6 @@ const fromBase = document.querySelector("#fromBase");
 const toBase = document.querySelector("#toBase");
 const fromField = document.querySelector("#fromField");
 const toField = document.querySelector("#toField");
-const convertButton = document.querySelector("#button");
 
 const conversions = {};
 conversions.BinToBin = bin => parseInt(bin, 2).toString(2);
@@ -16,17 +15,18 @@ conversions.HexToBin = hex => parseInt(hex, 16).toString(2);
 conversions.HexToDec = hex => parseInt(hex, 16).toString(10);
 conversions.HexToHex = hex => parseInt(hex, 16).toString(16).toUpperCase();
 
-form.addEventListener("submit", e => e.preventDefault());
-
-fromBase.addEventListener("change", () => {
-    fromField.value = "";
-    toField.value = "";
-});
-
-toBase.addEventListener("change", () => toField.value = "");
-
-convertButton.addEventListener("click", () => {
-    if (!fromField.value) return;
+function convert() {
     let conversion = fromBase.value + "To" + toBase.value;
     toField.value = conversions[conversion](fromField.value);
+}
+
+form.addEventListener("submit", e => e.preventDefault());
+
+fromBase.addEventListener("change", () => fromField.value = toField.value = "");
+
+toBase.addEventListener("change", () => convert());
+
+fromField.addEventListener("input", () => {
+    if (!fromField.value) toField.value = "";
+    else convert();
 });
